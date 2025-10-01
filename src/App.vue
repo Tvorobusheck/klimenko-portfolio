02c05778d9d7f5e67fc63bb946fa0a74d77f3558
Vladislav Klimenko <vlad991199@yandex.ru>
2025-10-01 10:28:13 +0300

    Initialized vue application

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,47 @@
 <script setup>
-// This will be our main app component
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-          <a class="navbar-brand" href="#">Портфолио Клименко</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#about">О себе</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#skills">Навыки</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#projects">Проекты</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contacts">Контакты</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <main>
-      <router-view></router-view>
-    </main>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
 
-    <footer class="bg-dark text-white text-center py-4">
-      <div class="container">
-        <p>&copy; 2023 Портфолио Клименко В.В. Все права защищены.</p>
-      </div>
-    </footer>
-  </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<style>
-/* Global styles */
-body {
-  padding-top: 56px;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+<style scoped>
+header {
+  line-height: 1.5;
 }
 
-/* Navigation */
-.navbar {
-  box-shadow: 0 2px 4px rgba(0,0,0,.1);
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-/* Main content */
-main {
-  min-height: calc(100vh - 180px);
-}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
 
-/* Footer */
-footer {
-  margin-top: auto;
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
-  server: {
-    port: 3000,
-    open: true
-  }
 })
